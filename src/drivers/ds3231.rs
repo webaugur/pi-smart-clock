432f7dec6388304c3d69e4daae891f0465ebf3b5
David L Norris <dave@webaugur.com>
2026-05-30 19:16:19 -0400

    Add DS3231 RTC driver

---
+++ src/drivers/ds3231.rs
@@ -0,0 +1,16 @@
+use crate::drivers::platform::Platform;
+
+pub struct DS3231;
+
+impl DS3231 {
+    pub const ADDR: u8 = 0x68;
+
+    pub async fn synchronize<P: Platform>(platform: &mut P) {
+        // Read time from DS3231 and set system time
+        println!("[DS3231] Time synchronized");
+    }
+
+    pub async fn set_time<P: Platform>(platform: &mut P, hour: u32, minute: u32) {
+        // Write time to DS3231
+    }
+}
