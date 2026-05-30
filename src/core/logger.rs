2883e03701cda65764b9c93900a261267e539d7d
David L Norris <dave@webaugur.com>
2026-05-30 19:23:16 -0400

    Add logging system (off by default)

---
+++ src/core/logger.rs
@@ -0,0 +1,17 @@
+use crate::drivers::platform::Platform;
+
+pub struct Logger {
+    pub enabled: bool,
+}
+
+impl Logger {
+    pub fn new() -> Self {
+        Self { enabled: false } // Off by default
+    }
+
+    pub async fn log<P: Platform>(&self, platform: &mut P, level: &str, message: &str) {
+        if !self.enabled { return; }
+        let timestamp = platform.get_current_time();
+        println!("[{}] {}: {}", timestamp.format("%H:%M:%S"), level, message);
+    }
+}
